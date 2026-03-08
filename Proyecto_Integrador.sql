264b4af540af3b5190d298272743ef1eee8b9c31
Yuliana-code28 <124048858@upq.edu.mx>
2026-03-08 12:35:36 -0600

    inserts bd y cambio en tabla

--- Proyecto_Integrador.sql
+++ Proyecto_Integrador.sql
@@ -95,3 +95,36 @@ create table detalle_salidas(
     foreign key (id_salida) references salidas(id_salida),
     foreign key (id_producto) references productos(id_producto)
 );
+
+alter table usuarios change contraseña contrasena varchar(50);
+insert into roles (nombre) values
+('Administrador'),
+('Encargado'),
+('Empleado');
+
+insert into areas (nombre) values
+('Papeleria'),
+('Bodega'),
+('Caja');
+
+insert into categorias (nombre) values
+('Cuadernos'),
+('Plumas'),
+('Hojas');
+insert into estado_producto (nombre_estado) values
+('Disponible'),
+('Agotado');
+
+insert into empleados
+(id_area, id_rol, nombre, ap, am, telefono, correo)
+values
+(1,1,'Jose Maria','Jimenez','Olvera','4421111111','chema@controlstock.com'),
+(1,2,'Victor Manuel','De Vicente','Atanacio','4422222222','victor@controlstock.com'),
+(1,3,'Sebastian','Martinez','Marcial','4423333333','sebas@controlstock.com');
+
+insert into usuarios
+(nombre_usuario, contrasena, ultima_modificacion, id_empleado)
+values
+('chema','admin123',curdate(),1),
+('victor','encargado123',curdate(),2),
+('sebas','empleado123',curdate(),3);
